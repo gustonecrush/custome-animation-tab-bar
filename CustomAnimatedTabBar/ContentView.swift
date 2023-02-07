5d7925bae14993a304c6077038e1cabc28a55052
gustonecrush <codeboxezz@gmail.com>
2023-02-07 23:32:46 +0700

    initialized project

---
+++ CustomAnimatedTabBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomAnimatedTabBar
+//
+//  Created by A Farhan Agustiansyah on 07/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomAnimatedTabBar/CustomAnimatedTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAnimatedTabBarApp.swift
+//  CustomAnimatedTabBar
+//
+//  Created by A Farhan Agustiansyah on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomAnimatedTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
